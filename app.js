71c88e76c82c09593e6da248c39aced7647c26ec
JediMindtrick <brandonjwilhite@gmail.com>
2014-10-30 05:20:39 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,26 @@
+/**
+ * Module dependencies.
+ */
+var express = require('express'),
+http = require('http'),
+path = require('path');
+
+app = express();
+
+// all environments
+app.set('port', process.env.PORT || 7070);
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.favicon());
+app.use(express.json(false));
+app.use(express.urlencoded());
+app.use(express.methodOverride());
+app.use(express.static(path.join(__dirname, 'public')));
+app.engine('html', require('ejs').renderFile);
+
+app.get('/', function(req, res) {
+    res.render('index.html');
+});
+
+server = http.createServer(app).listen(app.get('port'), function(){
+    console.log('Express server listening on port ' + app.get('port'));
+});
