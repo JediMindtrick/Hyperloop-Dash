06b613cfae432bb1a1cdb4ab78e8b5a84066dec9
Brandon Wilhite <brandonjwilhite@gmail.com>
2014-11-03 06:52:30 -0500

    initial stacked bar chart

--- public/SPA/analysis.js
+++ public/SPA/analysis.js
@@ -24,6 +24,31 @@ var getRangePartitions = function(len,by){
     return toReturn;
 };
 
+var getAverage = function(arr,selector){
+    var avgAcc = _.reduce(arr,
+        function(acc,point){
+            acc.count++;
+            acc.sum += selector(point);
+            return acc;
+        },{count: 0, sum: 0});
+
+    var average = avgAcc.sum / avgAcc.count;
+    return average;
+};
+
+/*
+TotalTime: evt.perfClientReceived - evt.perfClientSent,
+SendToStream: evt.perfStreamReceived - evt.perfClientSent,
+Between1: evt.perfBeginCommit - evt.perfStreamReceived,
+WriteToDisk: evt.perfEndCommit - evt.perfBeginCommit,
+Between2: evt.perfPublishEvent - evt.perfEndCommit,
+SendToBLP: evt.perfPublishReceived - evt.perfPublishEvent,
+BLPlogic: evt.perfSendToStore - evt.perfPublishReceived,
+SendToStore: evt.perfStoreReceived - evt.perfSendToStore,
+SetModel: evt.perfNotifyModelChange - evt.perfStoreReceived,
+SendToClient: evt.perfClientReceived - evt.perfNotifyModelChange,
+*/
+
 var getSet = function(arr){
     var toReturn = [];
 
@@ -31,16 +56,22 @@ var getSet = function(arr){
 
     toReturn = _.map(partitions,function(part,idx){
 
-        var avg = _.reduce(getRange(arr,part.begin,part.end),
-            function(acc,point){
-                acc.count++;
-                acc.sum += point.TotalTime;
-                return acc;
-            },{count: 0, sum: 0});
+        var _range = getRange(arr,part.begin,part.end);
+
+        var avgTotalTime = getAverage(_range,function(d){ return d.TotalTime; });
 
         return {
             partition: idx,
-            time: avg.sum / avg.count
+            sendToStream: getAverage(_range,function(d){ return d.SendToStream }),
+            between1: getAverage(_range,function(d){ return d.Between1 }),
+            writeToDisk: getAverage(_range,function(d){ return d.WriteToDisk }),
+            between2: getAverage(_range,function(d){ return d.Between2 }),
+            sendToBLP: getAverage(_range,function(d){ return d.SendToBLP }),
+            blpLogic: getAverage(_range,function(d){ return d.BLPlogic }),
+            sendToStore: getAverage(_range,function(d){ return d.SendToStore }),
+            setModel: getAverage(_range,function(d){ return d.SetModel }),
+            sendToClient: getAverage(_range,function(d){ return d.SendToClient }),
+            time: avgTotalTime
         };
     });
 
